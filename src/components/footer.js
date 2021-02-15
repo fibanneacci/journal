46e54efef7ca0b327a97cda10def19d854dd70bb
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-02-15 13:07:51 -0600

    css for links lol

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,7 +11,7 @@ const Footer = ({ siteTitle }) => (
     >
         © {new Date().getFullYear()}, Built with
         {` `}
-        <mark><a href="https://www.gatsbyjs.com" target="_blank" rel="noreferrer">Gatsby</a></mark>.
+        <a href="https://www.gatsbyjs.com" target="_blank" rel="noreferrer">Gatsby</a>.
     </footer>
 )
 
--- src/pages/404.js
+++ src/pages/404.js
@@ -9,7 +9,7 @@ const NotFoundPage = () => (
     <SEO title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    <mark><Link to="/">Get back on the map.</Link></mark>
+    <Link to="/">Get back on the map.</Link>
   </Layout>
 )
 
